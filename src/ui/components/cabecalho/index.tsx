5cd019a35098cfb808f52bd3d0bcf13fac8a73dd
Lucas <48070334+LucasRocha-01@users.noreply.github.com>
2022-06-03 00:23:59 -0300

    first commit

---
+++ src/ui/components/cabecalho/index.tsx
@@ -0,0 +1,9 @@
+import { Logo, CabecalhoContainer } from "./style";
+
+export default function Cabecalho() {
+  return (
+    <CabecalhoContainer>
+      <Logo src="/imagens/logo.svg" />
+    </CabecalhoContainer>
+  );
+}
