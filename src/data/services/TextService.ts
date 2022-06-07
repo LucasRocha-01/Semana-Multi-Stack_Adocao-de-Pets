aecf94d8518926ae195032eb4e8dd8760a8d1022
Lucas <48070334+LucasRocha-01@users.noreply.github.com>
2022-06-07 00:22:10 -0300

    front end

--- src/data/services/TextService.ts
+++ src/data/services/TextService.ts
@@ -1,7 +1,7 @@
 export const TextService = {
     limitarTexto(texto: string, tamanhoMaximo: number): string{
             if(texto.length < tamanhoMaximo) {
-                return texto
+                return texto;
             }
             return texto.slice(0, tamanhoMaximo) + '...' 
         } 
